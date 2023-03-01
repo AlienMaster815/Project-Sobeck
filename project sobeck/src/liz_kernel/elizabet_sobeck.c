2e72a7d3fb8a247df4f0bf7dfd2f1310afdb4f62
AlienMaster815 <126557457+AlienMaster815@users.noreply.github.com>
2023-02-28 20:38:29 -0500

    Add files via upload

--- project sobeck/src/liz_kernel/elizabet_sobeck.c
+++ project sobeck/src/liz_kernel/elizabet_sobeck.c
@@ -2,6 +2,6 @@
 #include "alpha_variables.h"
 #include "string.h"
 void elizabet_sobeck(){
-    liz_print_str("hello im elizebeth sobeck call me liz!");
+    liz_print_str("hello im elizabet sobeck call me liz!");
     while(1);
 }
